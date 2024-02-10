2298671da83dd9ce6f53563ba65ccea593b78132
Nilesh Ravindra Manke <nileshmanke12ab@gmail.com>
2024-02-10 13:29:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Product from "./components/Product";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Product/>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+
+    screens: {
+      'xs': '320px',     // Custom extra-small breakpoint
+      'sm': '780px',     // Custom small breakpoint
+      'md': '768px',     // Custom medium breakpoint
+      'lg': '1024px',    // Custom large breakpoint
+      'xl': '1280px',    // Custom extra-large breakpoint
+      '2xl': '1536px',   // Custom 2x-large breakpoint
+      // You can add more custom breakpoints as needed
+    },
+  },
+  plugins: [],
+};
